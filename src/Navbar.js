445502b0bcb71e5498fdc403431d01dde8884060
ritwikan <117129734+ritwikan@users.noreply.github.com>
2023-12-17 13:09:21 -0700

    Navbar styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./Navbar.css"; // Import CSS file for styling
 import { Link } from "react-router-dom";
+import "@fontsource/jetbrains-mono";
+import { IoPin } from "react-icons/io5";
+import { IoPersonSharp } from "react-icons/io5";
+import { LiaNetworkWiredSolid } from "react-icons/lia";
 
 function BottomNavbar() {
   return (
     <div>
       <div className="bottom-navbar">
-        <Link to="/map">Map</Link>
-        <Link to="/network">Network</Link>
-        <Link to="/profile">Profile</Link>
+        <Link to="/map">
+          <IoPin className="map" />
+        </Link>
+        <Link to="/network">
+          <LiaNetworkWiredSolid className="network" />
+        </Link>
+        <Link to="/profile">
+          <IoPersonSharp className="profile" />
+        </Link>
       </div>
     </div>
   );
