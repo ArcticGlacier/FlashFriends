6e254826ce9da2ca0bb8690f9b70bfd5aed8439d
ritwikan <117129734+ritwikan@users.noreply.github.com>
2023-12-17 21:54:31 -0700

    push

---
+++ src/Gallery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Gallery.css';
+
+funct Galley = () => {
+  return (
+    <div className="image-container">
+      <img
+        src="your-image-url.jpg"  // Replace with the URL or path to your image
+        alt="Your Image"
+        className="image"
+      />
+    </div>
+  );
+};
+
+export default ImagePage;
